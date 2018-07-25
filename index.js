1c91e82ff50405ad8aaa70dd9e53b19941b238db
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 14:21:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,37 +1,63 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+//
+// const driver = {};
+//
+// function updateDriverWithKeyAndValue(obj,key,value){
+//     let newDriver = {...obj};
+//
+//       return newDriver[key] = value;
+//
+// }
+//
+// function destructivelyUpdateDriverWithKeyAndValue(obj,key, value){
+//   return obj[key] = value;
+// }
+//
+//
+// function deleteFromDriverByKey(driver, key){
+//   let newDriver = {..obj};
+//   delete newDriver[key];
+// }
+//
+//
+// function destructivelyDeleteFromDriverByKey(driver,ley){
+//   return   delete newDriver[key]
+//
+//
+// }
+//  const driver = {}
+//
+//  function DriverWithKeyAndValue(driver, key, value){
+//     const newDriver = { ...driver };
+//
+//     newDriver[key]= value;
+//
+//     return newDriver;
+//
+//  }
 
 const driver = {};
 
-function updateDriverWithKeyAndValue(obj,key,value){
-    let newDriver = {...obj};
-
-      return newDriver[key] = value;
-
+function updateDriverWithKeyAndValue (driver, key, value) {
+  return Object.assign({}, driver, { [key]: value });
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(obj,key, value){
-  return obj[key] = value;
+function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
+  driver[key] = value;
+
+  return driver;
 }
 
+function deleteFromDriverByKey (driver, key) {
+  const newObj = Object.assign({}, driver);
 
-function deleteFromDriverByKey(driver, key){
-  let newDriver = {..obj};
-  delete newDriver[key];
+  delete newObj[key];
+
+  return newObj;
 }
 
+function destructivelyDeleteFromDriverByKey (driver, key) {
+  delete driver[key];
 
-function destructivelyDeleteFromDriverByKey(driver,ley){
-  return   delete newDriver[key]
-
-
+  return driver;
 }
- const driver = {}
-
- function DriverWithKeyAndValue(driver, key, value){
-    const newDriver = { ...driver };
-
-    newDriver[key]= value;
-
-    return newDriver;
-
- }

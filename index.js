a53b164a60bf6bf2caf930efd0c4cabe65a0ec86
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 14:21:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-// // Write your solution in this file!
-//
-// const driver = {};
-//
-// function updateDriverWithKeyAndValue(obj,key,value){
-//     let newDriver = {...obj};
-//
-//       return newDriver[key] = value;
-//
-// }
-//
-// function destructivelyUpdateDriverWithKeyAndValue(obj,key, value){
-//   return obj[key] = value;
-// }
-//
-//
-// function deleteFromDriverByKey(driver, key){
-//   let newDriver = {..obj};
-//   delete newDriver[key];
-// }
-//
-//
-// function destructivelyDeleteFromDriverByKey(driver,ley){
-//   return   delete newDriver[key]
-//
-//
-// }
-//  const driver = {}
-//
-//  function DriverWithKeyAndValue(driver, key, value){
-//     const newDriver = { ...driver };
-//
-//     newDriver[key]= value;
-//
-//     return newDriver;
-//
-//  }
+// Write your solution in this file!
+
+const driver = {};
+
+function updateDriverWithKeyAndValue(obj,key,value){
+    let newDriver = {...obj};
+
+      return newDriver[key] = value;
+
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj,key, value){
+  return obj[key] = value;
+}
+
+
+function deleteFromDriverByKey(driver, key){
+  let newDriver = {..obj};
+  delete newDriver[key];
+}
+
+
+function destructivelyDeleteFromDriverByKey(driver,ley){
+  return   delete newDriver[key]
+
+
+}
+ const driver = {}
+
+ function DriverWithKeyAndValue(driver, key, value){
+    const newDriver = { ...driver };
+
+    newDriver[key]= value;
+
+    return newDriver;
+
+ }

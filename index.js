dea3a2a19956fc300ba7ee72e9ebd3e9fea4b51d
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 14:34:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver,key, value){
 }
 
 function deleteFromDriverByKey(driver, key){
-  const newDriver = {..driver};
+  const newDriver = {..driver}
 //   delete newDriver.key;
 //   return newDriver
 }

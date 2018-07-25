5fe6255dbd35f8bbc8dff7658624e6dbd370393f
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 14:32:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ function updateDriverWithKeyAndValue(driver,key,value){
   return Object.assign({}, driver, {[key]: value} )
 }
 
-
-//
 function destructivelyUpdateDriverWithKeyAndValue(driver,key, value){
   driver[key] = value;
-  
+
   return driver
 }
-//
-//
-// function deleteFromDriverByKey(driver, key){
-//   let newDriver = {..obj};
-//   delete newDriver[key];
-// }
+
+function deleteFromDriverByKey(driver, key){
+  let newDriver = {..obj};
+  delete newDriver[key];
+  return newDriver
+}
 
 
 // function destructivelyDeleteFromDriverByKey(driver,ley){
